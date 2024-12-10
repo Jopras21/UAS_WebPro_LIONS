f8c4df610b95041e3f661ece3596fc19ac224435
Jopras21 <sun.phin098@gmail.com>
2024-12-11 00:27:31 +0700

    route for gallery

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\GalleryController;
 use app\Models\Calendar;
 
 Route::get('/', function () {
@@ -10,3 +11,6 @@ Route::get('/', function () {
 
 Route::resource('calendars', CalendarController::class);
 
+// gallery
+Route::resource('gallery', GalleryController::class);
+Route::post('/gallery/upload', [GalleryController::class, 'upload'])->name('gallery.upload');

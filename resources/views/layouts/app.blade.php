3396828e29581e107f887ddf26ae7fc63cbe4ccc
Jopras21 <sun.phin098@gmail.com>
2024-12-11 00:29:10 +0700

    template layout yang bakal dipake di setiap pages blade yang ada pake yang ini ya

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ $title }} | UMN Medical Center</title>
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.css">
+
+    <!-- Scripts -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+
+    <style>
+        * {
+            font-family: 'Bitter', serif;
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
+        .background-login {
+            background-image: url("{{ asset('assets/background/login.jpg') }}");
+            background-size: cover;
+            background-position: center;
+            bcakground-repeat: no-repeat;
+        }
+
+        .sticky {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            z-index: 1000;
+        }
+
+        .bg-gallery {
+            background-image: url("{{ asset('assets/background/white_n_green.png') }}");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+        }
+    </style>
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen">
+        @include('components.navigation')
+
+        <!-- Page Content -->
+        <main class="mx-auto px-0">
+            @yield('content')
+        </main>
+
+        @include('components.footer')
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            $('[data-fancybox="gallery"]').fancybox();
+        });
+    </script>
+</body>
+
+</html>
---
+++ resources/views/layouts/dashboard_template.blade.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/css/style.css">
+    <title>{{ $title }} | UMN Medical Center</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/dropzone.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
+
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.umd.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/dropzone.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+
+    <style>
+        * {
+            font-family: 'Bitter', serif;
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+    </style>
+</head>
+
+<body class="text-black">
+
+    <!-- start: Sidebar -->
+    <div class="sidebar-root"></div>
+    <!-- end: Sidebar -->
+
+    <!-- start: Main -->
+    <main class="w-full bg-gray-50 min-h-screen transition-all main">
+        <div class="navbar-root"></div>
+
+        @yield('content')
+
+        <div class="footer-root"></div>
+    </main>
+    <!-- end: Main -->
+
+    <script src="https://unpkg.com/@popperjs/core@2"></script>
+    <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/tabs.js"></script>
+    <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/collapse.js"></script>
+
+    <script>
+        // start: Sidebar
+        const sidebarToggle = document.querySelector('.sidebar-toggle')
+        const sidebarOverlay = document.querySelector('.sidebar-overlay')
+        const sidebarMenu = document.querySelector('.sidebar-menu')
+        const main = document.querySelector('.main')
+        const sidebarCloseBtn = document.querySelector('.sidebar-close');
+
+        if (window.innerWidth < 768) {
+            main.classList.toggle('active')
+            sidebarOverlay.classList.toggle('hidden')
+        }
+
+        sidebarToggle.addEventListener('click', function (e) {
+            e.preventDefault()
+            main.classList.toggle('active')
+            sidebarOverlay.classList.toggle('hidden')
+            sidebarMenu.classList.toggle('-translate-x-full')
+        })
+
+        sidebarOverlay.addEventListener('click', function (e) {
+            e.preventDefault()
+            main.classList.add('active')
+            sidebarOverlay.classList.add('hidden')
+            sidebarMenu.classList.add('-translate-x-full')
+        })
+
+        sidebarCloseBtn.addEventListener('click', function () {
+            sidebarMenu.classList.add('-translate-x-full');
+            sidebarOverlay.classList.add('hidden');
+        });
+
+        document.querySelectorAll('.sidebar-dropdown-toggle').forEach(function (item) {
+            item.addEventListener('click', function (e) {
+                e.preventDefault()
+                const parent = item.closest('.group')
+                if (parent.classList.contains('selected')) {
+                    parent.classList.remove('selected')
+                } else {
+                    document.querySelectorAll('.sidebar-dropdown-toggle').forEach(function (i) {
+                        i.closest('.group').classList.remove('selected')
+                    })
+                    parent.classList.add('selected')
+                }
+            })
+        })
+        // end: Sidebar
+
+        document.addEventListener('DOMContentLoaded', function () {
+            const greetingElement = document.getElementById('greeting');
+            const currentHour = new Date().getHours();
+            let greetingText;
+
+            if (currentHour >= 5 && currentHour < 12) {
+                greetingText = 'Good Morning,';
+            } else if (currentHour >= 12 && currentHour <= 15) {
+                greetingText = 'Good Afternoon,';
+            } else if (currentHour >= 16 && currentHour <= 18) {
+                greetingText = 'Good Evening,';
+            } else {
+                greetingText = 'Good Night,';
+            }
+
+            greetingElement.textContent = greetingText;
+        });
+
+
+
+        // start: Popper
+        const popperInstance = {}
+        document.querySelectorAll('.dropdown').forEach(function (item, index) {
+            const popperId = 'popper-' + index
+            const toggle = item.querySelector('.dropdown-toggle')
+            const menu = item.querySelector('.dropdown-menu')
+            menu.dataset.popperId = popperId
+            popperInstance[popperId] = Popper.createPopper(toggle, menu, {
+                modifiers: [
+                    {
+                        name: 'offset',
+                        options: {
+                            offset: [0, 8],
+                        },
+                    },
+                    {
+                        name: 'preventOverflow',
+                        options: {
+                            padding: 24,
+                        },
+                    },
+                ],
+                placement: 'bottom-end'
+            });
+        })
+
+        document.addEventListener('click', function (e) {
+            const toggle = e.target.closest('.dropdown-toggle')
+            const menu = e.target.closest('.dropdown-menu')
+            if (toggle) {
+                const menuEl = toggle.closest('.dropdown').querySelector('.dropdown-menu')
+                const popperId = menuEl.dataset.popperId
+                if (menuEl.classList.contains('hidden')) {
+                    hideDropdown()
+                    menuEl.classList.remove('hidden')
+                    showPopper(popperId)
+                } else {
+                    menuEl.classList.add('hidden')
+                    hidePopper(popperId)
+                }
+            } else if (!menu) {
+                hideDropdown()
+            }
+        })
+
+        function hideDropdown() {
+            document.querySelectorAll('.dropdown-menu').forEach(function (item) {
+                item.classList.add('hidden')
+            })
+        }
+
+        function showPopper(popperId) {
+            popperInstance[popperId].setOptions(function (options) {
+                return {
+                    ...options,
+                    modifiers: [
+                        ...options.modifiers,
+                        { name: 'eventListeners', enabled: true },
+                    ],
+                }
+            });
+            popperInstance[popperId].update();
+        }
+
+        function hidePopper(popperId) {
+            popperInstance[popperId].setOptions(function (options) {
+                return {
+                    ...options,
+                    modifiers: [
+                        ...options.modifiers,
+                        { name: 'eventListeners', enabled: false },
+                    ],
+                }
+            });
+        }
+        // end: Popper
+
+
+
+        // start: Tab
+        document.querySelectorAll('[data-tab]').forEach(function (item) {
+            item.addEventListener('click', function (e) {
+                e.preventDefault()
+                const tab = item.dataset.tab
+                const page = item.dataset.tabPage
+                const target = document.querySelector('[data-tab-for="' + tab + '"][data-page="' + page + '"]')
+                document.querySelectorAll('[data-tab="' + tab + '"]').forEach(function (i) {
+                    i.classList.remove('active')
+                })
+                document.querySelectorAll('[data-tab-for="' + tab + '"]').forEach(function (i) {
+                    i.classList.add('hidden')
+                })
+                item.classList.add('active')
+                target.classList.remove('hidden')
+            })
+        })
+        // end: Tab
+    </script>
+</body>
+
+</html>

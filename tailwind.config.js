14a03075f53bf3f2bedcdfce67a8069334eac68a
Jopras21 <sun.phin098@gmail.com>
2024-12-11 21:24:54 +0700

    packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // For React files
+    "./resources/views/**/*.blade.php", // For Laravel Blade templates
+    "./resources/js/**/*.js", // For Laravel's JavaScript files
+    "./storage/framework/views/*.php", // Cached Blade views
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php", // Laravel Pagination
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require('@tailwindcss/forms')],
+};

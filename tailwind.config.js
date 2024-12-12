0e18bf74211db619c0ee5e8ce5d33ea6349987bd
Cryoonic <albertalexander2004@gmail.com>
2024-12-12 13:42:33 +0700

    ....

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};

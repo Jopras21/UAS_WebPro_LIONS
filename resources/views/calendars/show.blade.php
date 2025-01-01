c62836e3da2ff28b2261bf001afcb651684377ec
Abigail Vania <abigailvaniapra@gmail.com>
2025-01-01 23:53:09 +0700

    calendar show fixed

--- resources/views/calendars/show.blade.php
+++ resources/views/calendars/show.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
-@section('title', 'Calendar')
+@section('content')
 
-<div class="container mx-auto p-4">
+<div class="container mx-auto p-4 text-white">
     <h1 class="text-2xl font-bold mb-4">All Events</h1>
     
     @if($events->isEmpty())

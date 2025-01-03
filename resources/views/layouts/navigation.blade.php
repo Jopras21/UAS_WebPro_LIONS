90c33cfcbe2805d1dd08ca5db434c99dc12b9f11
foodkin <jasonjeferson81@gmail.com>
2025-01-03 18:15:29 +0700

    Nav nya ke push tadi

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,37 +12,56 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('gallery.index')" :active="request()->routeIs('gallery.index')">
-                        {{ __('Gallery') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('calendars.index')" :active="request()->routeIs('calendars.index')">
-                        {{ __('Calendar') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('matches.show')" :active="request()->routeIs('matches.show')">
-                        {{ __('Matches') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('members.index')" :active="request()->routeIs('members.index')">
-                        {{ __('Member') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('contact.index')" :active="request()->routeIs('contact.index')">
-                        {{ __('Contact') }}
-                    </x-nav-link>
-                    
-                    <!-- Logout Button -->
-                    @auth
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <button type="submit" class="text-white hover:bg-gray-600 px-4 py-2 rounded-md">
-                            {{ __('Log Out') }}
-                        </button>
-                    </form>
-                    @endauth
+                        <x-nav-link :href="route('gallery.index')" :active="request()->routeIs('gallery.index')">
+                            {{ __('Gallery') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('calendars.index')" :active="request()->routeIs('calendars.index')">
+                            {{ __('Calendar') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('matches.show')" :active="request()->routeIs('matches.show')">
+                            {{ __('Matches') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('members.index')" :active="request()->routeIs('members.index')">
+                            {{ __('Member') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('contact.index')" :active="request()->routeIs('contact.index')">
+                            {{ __('Contact') }}
+                        </x-nav-link>
                 </div>
             </div>
 
-            <!-- Settings Dropdown (Optional, for extra settings) -->
+            <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <!-- Here you can add any other settings or user profile dropdown -->
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white bg-gradient-to-r from-[#324582] to-[#354989] hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <div>{{ Auth::check() ? Auth::user()->name : 'Guest' }}</div>
+
+                            <div class="ms-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
             </div>
 
             <!-- Hamburger -->
